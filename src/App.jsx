840fc80e25425394eb4379250907003f4570487a
Akuila-dok <miyomdok@gmail.com>
2024-04-28 18:53:24 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './assets/Pages/Home';
 import Programpage from './assets/Pages/Programpage';
