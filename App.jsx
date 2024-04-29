6e3b606e5a8403168fc1698c6faccd7ad612fbc9
Akuila-dok <miyomdok@gmail.com>
2024-04-29 17:52:44 +0300

    first commit

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './assets/Pages/Home';
-import Programpage from './assets/Pages/Programpage';
-import Admissioncriteria from './assets/Pages/Admissioncriteria';
-import Programs from './assets/Components/Programs/Programs';
+import Home from './src/assets/Pages/Home';
+import Programpage from './src/assets/Pages/Programpage';
+import Admissioncriteria from './src/assets/Pages/Admissioncriteria';
+import Programs from './src/assets/Components/Programs/Programs';
 
 function App() {
   return (
